fbca3db9335dd6c661a5c8d4fd460e39c25a29fe
junaid-12junaid <junaidullakhan619@gmail.com>
2023-02-19 16:42:32 +0530

    Done

--- client/src/components/filesUI/FileList.jsx
+++ client/src/components/filesUI/FileList.jsx
@@ -3,16 +3,6 @@ import FilesPage from '../../pages/FilesPage'
 
 export default function FileList({files}) {
   
-let copy=files
-let [files1,setFiles1]=useState(files)
-
-  let deleteRow=(num)=>{
-    let copy=[...files1]
-    console.log({copy:num})
-   let ind= copy.findIndex((ele)=>ele.filename==num)
-      copy.splice(ind,1)
-      setFiles1(copy)
-  }
 
   return (
     <div>
@@ -30,13 +20,13 @@ let [files1,setFiles1]=useState(files)
               </thead>
           <tbody>
           {
-                files1.map((file,index)=>{
+                files.map((file,index)=>{
                     return (
                     <tr key={file._id}>
                     <td>{index+1}</td>
                         <td>{file.filename}</td>
                         <td><button type="button" className="btn btn-info">View</button></td>
-                        <td><button type="button" className="btn btn-danger" onClick={()=>deleteRow(file.filename)}>Delete</button></td>
+                        <td><button type="button" className="btn btn-danger" >Delete</button></td>
                         
                         </tr>
                     )
